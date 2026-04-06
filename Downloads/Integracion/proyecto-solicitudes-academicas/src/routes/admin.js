1aec38a2da29ae36d4b9d2a9b20cbfda42091251
Lino Robechi <linorobechi06@gmail.com>
2026-04-05 23:23:29 -0500

    Eliminé todo el contenido

--- Downloads/Integracion/proyecto-solicitudes-academicas/src/routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const SolicitudController = require('../controllers/SolicitudController');
-
-// Estadísticas del sistema
-router.get('/sistema/estadisticas', (req, res) => SolicitudController.obtenerEstadisticas(req, res));
-
-module.exports = router;
